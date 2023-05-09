cd69e975eddc0a7390afeaa40ec3c7950ed8c60f
sunghoon <tjdgns_573@naver.com>
2023-05-09 18:12:46 +0900

    "sts 연습"

--- ch3/src/main/java/com/spring/dependency/TVMain.java
+++ ch3/src/main/java/com/spring/dependency/TVMain.java
@@ -11,13 +11,12 @@ public class TVMain {
 		
 		// TV tv =  (TV) ctx.getBean("samsungTV");   @Componet 로 생성시 id는 클래스명 사용(앞자리는 소문자)
 		TV tv =  (TV) ctx.getBean("tv");  //@Componet 
-
+		
+		
 		tv.powerOn();
 		tv.volumeUp();
 		tv.volumeDown();
 		tv.powerOff();
-
-		
 		
 	}
 
