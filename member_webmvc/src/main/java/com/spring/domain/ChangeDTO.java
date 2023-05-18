fae6709a32ea8c684d4ea0d69ae9bac646871895
sunghoon <tjdgns_573@naver.com>
2023-05-18 18:46:19 +0900

    "Board 게시판 만들기"

---
+++ member_webmvc/src/main/java/com/spring/domain/ChangeDTO.java
@@ -0,0 +1,23 @@
+package com.spring.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter @ToString
+public class ChangeDTO {
+// changePwd.jsp ¿Í ÀÏÄ¡
+	private String userid;
+	
+	private String currentPassword;
+	private String newPassword;
+	private String confirmPassword;
+	
+	public boolean passwordEquals() {
+		return newPassword.equals(confirmPassword);
+	}
+}
--- member_webmvc/src/main/java/com/spring/service/MemberServiceImpl.java
+++ member_webmvc/src/main/java/com/spring/service/MemberServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.spring.domain.AuthDTO;
+import com.spring.domain.ChangeDTO;
 import com.spring.domain.LoginDTO;
 import com.spring.domain.MemberDTO;
 import com.spring.mapper.MemberMapper;
@@ -43,6 +44,36 @@ public class MemberServiceImpl implements MemberService {
 		memberDTO.setPassword(bPasswordEncoder.encode(memberDTO.getPassword()));
 		
 		return mapper.insert(memberDTO)==1?true:false;
+	}
+
+	@Override
+	public boolean dupId(String userid) {
+		return mapper.dupId(userid) > 0 ? true:false;
+	}
+
+	@Override
+	public boolean remove(LoginDTO loginDTO) {
+		// 비밀번호 확인
+		String encodePass = mapper.getPass(loginDTO.getUserid());
+		
+		if(bPasswordEncoder.matches(loginDTO.getPassword(), encodePass)) {
+			return mapper.leave(loginDTO.getUserid())==1?true:false;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean update(ChangeDTO changeDTO) {
+		// 현재 비밀번호 확인
+		String encodePass = mapper.getPass(changeDTO.getUserid());
+		
+		if(bPasswordEncoder.matches(changeDTO.getCurrentPassword(), encodePass)) {
+			// 변경 비밀번호 암호화
+			changeDTO.setNewPassword(bPasswordEncoder.encode(changeDTO.getNewPassword()));
+			
+			return mapper.update(changeDTO)==1?true:false;
+		}
+		return false;
 	}
 
 }
